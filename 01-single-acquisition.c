f46304f52881238730f0fc9b19e919b63fd1c06a
Joey Sachtleben <jsachtleben@carthage.edu>
2022-06-09 12:00:53 -0500

    added more detail to example 01

--- 01-single-acquisition.c
+++ 01-single-acquisition.c
@@ -36,6 +36,17 @@ main (int argc, char **argv)
 			printf ("Acquired %d×%d buffer\n",
 				arv_buffer_get_image_width (buffer),
 				arv_buffer_get_image_height (buffer));
+			printf("Buffer information: \n");
+			printf("Buffer status: %d\n",
+				arv_buffer_get_status(buffer));
+			printf("Buffer payload type: %d\n",
+				arv_buffer_get_payload_type(buffer));
+			printf("Buffer image data format: 0x%x\n",
+				arv_buffer_get_image_pixel_format(buffer));
+			size_t dataSize;
+			guint8* bufferData = (guint8*)arv_buffer_get_data(buffer,&dataSize);
+			printf("Raw buffer data: %.*x",
+				dataSize, bufferData);
 			/* Destroy the buffer */
 			g_clear_object (&buffer);
 		}
