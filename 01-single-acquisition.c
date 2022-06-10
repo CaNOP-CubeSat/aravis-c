4f1255d2100f5ce6009c47409743982ba38beeea
Caleb C <ccollar@carthage.edu>
2022-06-10 12:48:43 -0500

    [CANOP-12] Added ImageMagick subproject.

--- 01-single-acquisition.c
+++ 01-single-acquisition.c
@@ -9,6 +9,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* Imagemagick headers */
+#include <ImageMagick-7/MagickWand/MagickWand.h>
+
 /*
  * Connect to the first available camera, then acquire a single buffer.
  */
@@ -33,9 +36,10 @@ main (int argc, char **argv)
 
 		if (ARV_IS_BUFFER (buffer)) {
 			/* Display some informations about the retrieved buffer */
+			gint imgWidth = arv_buffer_get_image_width (buffer);
+			gint imgHeight = arv_buffer_get_image_height (buffer);
 			printf ("Acquired %d×%d buffer\n",
-				arv_buffer_get_image_width (buffer),
-				arv_buffer_get_image_height (buffer));
+				imgWidth, imgHeight);
 			printf("Buffer information: \n");
 			printf("Buffer status: %d\n",
 				arv_buffer_get_status(buffer));
@@ -47,6 +51,19 @@ main (int argc, char **argv)
 			guint8* bufferData = (guint8*)arv_buffer_get_data(buffer,&dataSize);
 			printf("Raw buffer data: %.*x",
 				dataSize, bufferData);
+
+			/* Send buffer to image file */
+			//MagickConstituteImage() -> ConstituteImage() -> ImportImagePixels()-> ImportCharPixel().
+			
+			MagickWand *wand = NewMagickWand();
+			MagickConstituteImage(wand,imgWidth,imgHeight,"I",CharPixel,buffer);
+			MagickSetImageDepth(wand, 8);
+			//MagickSetImageColorspace(wand, Mono);
+			MagickSetImageFormat(wand, "jpg");
+			MagickSetImageExtent(wand, imgWidth,imgHeight);
+			MagickWriteImage(wand, "output.jpg");
+			ClearMagickWand(wand);
+
 			/* Destroy the buffer */
 			g_clear_object (&buffer);
 		}
